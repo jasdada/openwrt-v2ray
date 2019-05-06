05d99efaad4a157170adcc253613684514c8d33f
jasdada <12887817+jsda@users.noreply.github.com>
2019-05-07 04:58:44 +0800

    Update tag.sh

--- tag.sh
+++ tag.sh
@@ -1,3 +1,1 @@
-(curl --silent "https://api.github.com/repos/v2ray/v2ray-core/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/') > "/home/travis/build/tag.txt" && \
-TRAVIS_TAG=$(cat "/home/travis/build/tag.txt") && \
-echo "v2ray releases $TRAVIS_TAG"
+(curl --silent "https://api.github.com/repos/v2ray/v2ray-core/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/') > "$TRAVIS_BUILD_DIR/tag.txt"
