acd39f20af24bcc76bc2a219ed58dab1fb6d6a46
jasdada <12887817+jsda@users.noreply.github.com>
2019-05-08 00:54:25 +0800

    Delete tag.sh

--- tag.sh
+++
@@ -1,1 +0,0 @@
-rm -rf $TRAVIS_BUILD_DIR/tag.txt && (curl --silent "https://api.github.com/repos/v2ray/v2ray-core/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/') > "$TRAVIS_BUILD_DIR/tag.txt"
